981ceeba4ae5a812df8bfbed53c4557dc32fcb6a
PitiTheo <julio.vilaboa@yandex.com>
2021-04-27 02:59:39 +0000

    in progress dashboard

--- src/js/component/dashboard.js
+++ src/js/component/dashboard.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Navbar, Nav, Form, FormControl, Button, Card } from "react-bootstrap";
+import {
+	Navbar,
+	Nav,
+	Form,
+	FormControl,
+	Button,
+	Card,
+	Col
+} from "react-bootstrap";
 
 export function Dashboard() {
 	return (
@@ -30,42 +38,138 @@ export function Dashboard() {
 			<br />
 			<Card style={{ width: "50rem", margin: "auto" }}>
 				<Card.Body>
-					<Card.Header>Create profile</Card.Header>
+					<Card.Header>Create profile - Buddy</Card.Header>
 
 					<br />
 
 					<Card.Text>
+						<Form.Group controlId="formBasicEmail">
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type="name"
+								placeholder="Enter name"
+							/>
+						</Form.Group>
+
+						<Form.Group controlId="formBasicPassword">
+							<Form.Label>Lastname</Form.Label>
+							<Form.Control
+								type="lastname"
+								placeholder="Enter Lastname"
+							/>
+
+							<br />
+
+							<Form.Label style={{ marginBottom: "20px" }}>
+								Date of birth
+							</Form.Label>
+
+							<Form.Row>
+								<Form.Group as={Col} controlId="formGridMonth">
+									<Form.Label>Month</Form.Label>
+									<Form.Control
+										as="select"
+										defaultValue="Choose a month">
+										<option>Jan</option>
+										<option>Feb</option>
+										<option>Mar</option>
+										<option>Apr</option>
+										<option>May</option>
+										<option>Jun</option>
+										<option>Jul</option>
+										<option>Aug</option>
+										<option>Sep</option>
+										<option>Oct</option>
+										<option>Nov</option>
+										<option>Dec</option>
+									</Form.Control>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridMonth">
+									<Form.Label>Month</Form.Label>
+									<Form.Control
+										as="select"
+										defaultValue="Choose a month">
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+										<option>6</option>
+										<option>7</option>
+										<option>8</option>
+										<option>9</option>
+										<option>10</option>
+										<option>11</option>
+										<option>12</option>
+										<option>13</option>
+										<option>14</option>
+										<option>15</option>
+										<option>16</option>
+										<option>17</option>
+										<option>18</option>
+										<option>19</option>
+										<option>20</option>
+										<option>21</option>
+										<option>22</option>
+										<option>23</option>
+										<option>24</option>
+										<option>25</option>
+										<option>26</option>
+										<option>27</option>
+										<option>28</option>
+										<option>29</option>
+										<option>30</option>
+										<option>31</option>
+									</Form.Control>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridYear">
+									<Form.Label>Year</Form.Label>
+
+									<Form.Control />
+								</Form.Group>
+							</Form.Row>
+						</Form.Group>
+						<Form.Group controlId="formBasicPassword">
+							<Form.Label>Apellidos</Form.Label>
+							<Form.Control
+								type="password"
+								placeholder="Password"
+							/>
+						</Form.Group>
 						<Form>
-							<Form.Group controlId="formBasicCheckbox">
-								<Form.Check type="checkbox" label="Owner" />
-							</Form.Group>
+							<br />
+							<Form>
+								<Form.Label style={{ marginBottom: "20px" }}>
+									Accepted weight for pet
+								</Form.Label>
 
-							<Form.Group controlId="formBasicCheckbox">
-								<Form.Check type="checkbox" label="Buddy" />
-							</Form.Group>
+								{["checkbox"].map(type => (
+									<div
+										key={`inline-${type}`}
+										className="mr-6">
+										<Form.Check
+											inline
+											label="2-10 lbs"
+											type={type}
+											id={`inline-${type}-1`}
+										/>
+										<Form.Check
+											inline
+											label="10-15 lbs"
+											type={type}
+											id={`inline-${type}-2`}
+										/>
+										<Form.Check
+											inline
+											label="+25 lbs"
+											type={type}
+											id={`inline-${type}-3`}
+										/>
+									</div>
+								))}
 
-							<Form.Group controlId="formBasicEmail">
-								<Form.Label>Email address</Form.Label>
-								<Form.Control
-									type="email"
-									placeholder="Enter email"
-								/>
-							</Form.Group>
-
-							<Form.Group controlId="formBasicPassword">
-								<Form.Label>Nombre</Form.Label>
-								<Form.Control
-									type="password"
-									placeholder="Password"
-								/>
-							</Form.Group>
-							<Form.Group controlId="formBasicPassword">
-								<Form.Label>Apellidos</Form.Label>
-								<Form.Control
-									type="password"
-									placeholder="Password"
-								/>
-							</Form.Group>
+								<br />
+							</Form>
 
 							<Form.Group controlId="exampleForm.ControlSelect1">
 								<Form.Label>Provincia</Form.Label>
